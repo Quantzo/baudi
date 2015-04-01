f1c9ff860d065f5a042d717b6204e52c076aa47d
MidasPL <midas93@wp.pl>
2015-04-01 23:19:03 +0200

    First Commit GUI

---
+++ GUIBD/GUIBD/OknoEdycjiPracownika.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for OknoEdycjiPracownika.xaml
+    /// </summary>
+    public partial class OknoEdycjiPracownika : Window
+    {
+        public OknoEdycjiPracownika()
+        {
+            InitializeComponent();
+        }
+        private void CheckBoxChanged(object sender, RoutedEventArgs e)
+        {
+            if (JestMieszkancemBox.IsChecked.Value)
+            {
+                WyborMieszkancaLabel.IsEnabled = true;
+                WyborMieszkancaBox.IsEnabled = true;
+            }
+            else
+            {
+                WyborMieszkancaLabel.IsEnabled = false;
+                WyborMieszkancaBox.IsEnabled = false;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ GUIBD/GUIBD/ReallyMainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for ReallyMainWindow.xaml
+    /// </summary>
+    public partial class ReallyMainWindow : Window
+    {
+        public ReallyMainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OknoEdycjiZgloszenia OE = new OknoEdycjiZgloszenia();
+            OE.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OknoEdycjiFirmy OF = new OknoEdycjiFirmy();
+            OF.Show();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            OknoEdycjiPracownika OP = new OknoEdycjiPracownika();
+            OP.Show();
+        }
+    }
+}
