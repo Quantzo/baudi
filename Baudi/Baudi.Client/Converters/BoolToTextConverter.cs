bdf8aa6de260f97bb2807f68f56f5f6e50634c03
Mateusz Piątkowski <Quantzo@live.com>
2015-07-13 08:06:37 +0200

    bool to "tak" and "nie"

---
+++ Baudi/Baudi.Client/Converters/BoolToTextConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Baudi.Client.Converters
+{
+    public class BoolToTextConverter : IValueConverter
+    {
+
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var boolValue = value is bool && (bool) value;
+
+        return boolValue ? "Tak" : "Nie";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value != null && value.ToString() == "Tak";
+    }
+
+    }
+}
