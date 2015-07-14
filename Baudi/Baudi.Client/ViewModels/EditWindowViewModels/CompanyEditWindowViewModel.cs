87b50ba24525af02384d73bee843c011b22a65c9
Mateusz Piątkowski <Quantzo@live.com>
2015-07-14 14:57:14 +0200

    company edit winodow fix

---
+++ Baudi/Baudi.Client/ViewModels/EditWindowViewModels/CompanyEditWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Input;
+using Baudi.Client.View.EditWindows;
+using Baudi.DAL;
+using Baudi.DAL.Models;
+using Baudi.Client.ViewModels.TabsViewModels;
+using Baudi.Client.ViewModels.EditWindowViewModels;
+
+namespace Baudi.Client.ViewModels.EditWindowCode
+{
+    public class CompanyEditWindowViewModel : EditWindowViewModel
+    {
+        private Company _company;
+        public Company Company
+        {
+            get
+            {
+                return _company;
+            }
+            set
+            {
+                _company = value;
+                OnPropertyChanged("Comapny");
+            }
+               
+        }
+        public CompanyEditWindowViewModel(CompaniesTabViewModel companiesTabViewModel, CompanyEditWindow companyEditWindow, Company company)
+            : base(companiesTabViewModel, companyEditWindow)
+        {
+            Company = company;
+        }
+
+
+
+        public override void Save()
+        {
+         if(Company.CompanyID == 0)
+         {
+             using (var con = new BaudiDbContext())
+             {
+                 con.Companies.Add(Company);
+                 con.SaveChanges();
+             }
+         }
+         else
+         {
+             using (var con = new BaudiDbContext())
+             {
+                 var company = con.Companies.Find(Company.CompanyID);
+                 company.City = Company.City;                 
+                 company.HouseNumber = Company.HouseNumber;
+                 company.LocalNumber = Company.LocalNumber;
+                 company.Name = Company.Name;
+                 company.NIP = Company.NIP;
+                 company.Owner = Company.Owner;
+                 company.Street = Company.Street;
+                 company.TelephoneNumber = Company.TelephoneNumber;
+                 con.SaveChanges();
+             }
+         }
+
+            ParentViewModel.Update();
+            CloseWindow();
+        }
+    }
+}
