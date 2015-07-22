862452e71f9dcc5bbc8c7cdb8a5b01672d25abe5
Quantzo <Quantzo@live.com>
2015-07-22 23:58:39 +0200

    edit window views

---
+++ Baudi/Baudi.Client/View/EditWindows/OwningCompanyEditWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Baudi.Client.View.EditWindows
+{
+    /// <summary>
+    /// Interaction logic for OwningCompanyEditWindow.xaml
+    /// </summary>
+    public partial class OwningCompanyEditWindow : Window
+    {
+        public OwningCompanyEditWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
