5dd60f8cd4f7a638150ef6363d4cd4b28ef6b2c6
Mateusz Piątkowski <Quantzo@live.com>
2015-07-24 14:44:22 +0200

    rent edit window done

--- Baudi/Baudi.Client/ViewModels/EditWindowViewModels/RentEditWindowViewModel.cs
+++ Baudi/Baudi.Client/ViewModels/EditWindowViewModels/RentEditWindowViewModel.cs
@@ -61,17 +61,67 @@ namespace Baudi.Client.ViewModels.EditWindowViewModels
         public RentEditWindowViewModel(RentsTabViewModel rentsTabViewModel, RentEditWindow rentEditWindow, Rent rent)
             : base(rentsTabViewModel, rentEditWindow, rent)
         {
-
+            using (var con = new BaudiDbContext())
+            {
+                OwnershipsList = con.Ownerships.ToList();
+                if (Update)
+                {
+                    Rent = con.Rents.Find(rent.PaymentID);
+                    SelectedOwnership = Rent.Ownership;
+                }
+                else
+                {
+                    Rent = new Rent();
+                }
+            }
         }
 
         public override bool IsValid()
         {
-            throw new NotImplementedException();
+            if(SelectedOwnership != null)
+            {
+                return true;
+            }
+            return false;
         }
 
         public override void Save()
         {
-            throw new NotImplementedException();
+            if (Update)
+            {
+                using (var con = new BaudiDbContext())
+                {
+                    var ownership = con.Ownerships.Find(SelectedOwnership.OwnershipID);
+
+
+
+                    var rent = con.Rents.Find(Rent.PaymentID);
+                    rent.Ownership = ownership;
+                    rent.Date = Rent.Date;
+                    rent.Cost = Rent.Cost;
+                    rent.Paid = Rent.Paid;
+
+
+                    con.Entry(rent).State = EntityState.Modified;
+
+                    con.SaveChanges();
+                }
+
+            }
+            else
+            {
+                using (var con = new BaudiDbContext())
+                {
+                    var ownership = con.Ownerships.Find(SelectedOwnership.OwnershipID);
+
+                    Rent.Ownership = ownership;
+
+                    con.Rents.Add(Rent);
+                    con.SaveChanges();
+                }
+            }
+            ParentViewModel.Update();
+            CloseWindow();
         }
     }
 }
