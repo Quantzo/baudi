2b158004fb71a13f87351978a5e83f5c6da562b1
Mateusz Piątkowski <Quantzo@live.com>
2015-07-24 14:56:52 +0200

    salary edit window done

--- Baudi/Baudi.Client/ViewModels/EditWindowViewModels/SalaryEditWindowViewModel.cs
+++ Baudi/Baudi.Client/ViewModels/EditWindowViewModels/SalaryEditWindowViewModel.cs
@@ -28,8 +28,8 @@ namespace Baudi.Client.ViewModels.EditWindowViewModels
 
         }
 
-        private List<Ownership> _menagersList;
-        public List<Ownership> MenagersList
+        private List<Menager> _menagersList;
+        public List<Menager> MenagersList
         {
             get
             {
@@ -55,22 +55,110 @@ namespace Baudi.Client.ViewModels.EditWindowViewModels
                 OnPropertyChanged("SelectedMenager");
             }
         }
-        #endregion
 
-        public SalaryEditWindowViewModel(SalariesTabViewModel salaryTabViewModel, SalaryEditWindow salaryEditWindow, Salary salary)
+
+        private List<Employee> _employeesList;
+        public List<Employee> EmployeesList
+        {
+            get
+            {
+                return _employeesList;
+            }
+
+            set
+            {
+                _employeesList = value;
+                OnPropertyChanged("EmployeesList");
+            }
+        }
+        private Employee _selectedEmployee;
+        public Employee SelectedEmployee
+        {
+            get
+            {
+                return _selectedEmployee;
+            }
+            set
+            {
+                _selectedEmployee = value;
+                OnPropertyChanged("SelectedEmployee");
+            }
+        }
+            #endregion
+
+            public SalaryEditWindowViewModel(SalariesTabViewModel salaryTabViewModel, SalaryEditWindow salaryEditWindow, Salary salary)
             : base(salaryTabViewModel, salaryEditWindow, salary)
         {
 
+            using (var con = new BaudiDbContext())
+            {
+                MenagersList = con.Menagers.ToList();
+                EmployeesList = con.Employees.ToList();
+                if (Update)
+                {
+                    Salary = con.Salaries.Find(salary.PaymentID);
+                    SelectedMenager = Salary.Menager;
+                    SelectedEmployee = Salary.Employee;
+                }
+                else
+                {
+                    Salary = new Salary();
+                }
+            }
         }
 
         public override bool IsValid()
         {
-            throw new NotImplementedException();
+            if(SelectedMenager != null && SelectedEmployee != null)
+            {
+                return true;
+            }
+            return false;
         }
 
         public override void Save()
         {
-            throw new NotImplementedException();
+            if (Update)
+            {
+                using (var con = new BaudiDbContext())
+                {
+                    var menager = con.Menagers.Find(SelectedMenager.OwnerID);
+                    var employee = con.Employees.Find(SelectedEmployee.OwnerID);
+
+
+
+                    var salary = con.Salaries.Find(Salary.PaymentID);
+
+                    salary.Menager = menager;
+                    salary.Employee = employee;
+
+                    salary.Date = Salary.Date;
+                    salary.Cost = Salary.Cost;
+                    salary.Paid = Salary.Paid;
+
+
+                    con.Entry(salary).State = EntityState.Modified;
+
+                    con.SaveChanges();
+                }
+
+            }
+            else
+            {
+                using (var con = new BaudiDbContext())
+                {
+                    var menager = con.Menagers.Find(SelectedMenager.OwnerID);
+                    var employee = con.Employees.Find(SelectedEmployee.OwnerID);
+
+                    Salary.Menager = menager;
+                    Salary.Employee = employee;
+
+                    con.Salaries.Add(Salary);
+                    con.SaveChanges();
+                }
+            }
+            ParentViewModel.Update();
+            CloseWindow();
         }
     }
 }
